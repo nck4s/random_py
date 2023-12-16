667da2247aab4555479167abe81278cf1ef35017
nck4s <nn.2nn@mail.ru>
2023-12-16 12:05:43 +1000

    digit_sum improved

--- digit_sum.py
+++ digit_sum.py
@@ -1,7 +1,11 @@
-number = int(input('Enter a number: '))
-digit_sum = 0
-while number>0:
-    digit_sum += number % 10
-    number //= 10
+def sum_digits(number):
+    digit_sum = 0
+    while number > 0:
+        last_digit = number % 10
+        digit_sum += last_digit
+        number = number // 10
+    return digit_sum
 
-print(f'{digit_sum}')
+user_number = int(input('Enter a number: '))
+digit_sum = sum_digits(user_number)
+print(f'The sum of digits in {user_number} is: {digit_sum}')
